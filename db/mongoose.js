26b267ac8bf49baf734be7f728718e1fc8f1f9c8
Carlos Galaviz <carlos@email.com>
2022-10-04 18:16:18 -0600

    data

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }).then((data) =>  {
+  console.log(data);
+  console.log('Conectado al server');
+});
 
 module.exports = { mongoose };
